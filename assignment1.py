b44bac0fe9093f42cd59ac47f2323abb26e45655
Hien Truong <hien.truong2002.ht@gmail.com>
2023-01-26 08:58:46 +0100

    assignment1, Exercise 1a

--- assignment1.py
+++ assignment1.py
@@ -4,13 +4,35 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def f(x):
+    return x**4 - x**3 -10
+#Given initial interval [a,b] such that f(a)f(b) < 0, n is number of iterations
+def bisection_method(a, b, n):
+    condition = True
+    i = 1
+    while condition:
+        x = (a+b)/2
+        if f(x) < 0:
+            a = x
+        else:
+            b = x
+        if i == n:
+            condition = False
+        else:
+            condition = True
+            i = i + 1
+    print("Required root is: ", x)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+a = input("First approximation root: ")
+b = input("Second approximation root: ")
+n = input("Input number of iterations")
+a = float(a)
+b = float(b)
+n = int(n)  #number of iterations
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if f(a)*f(b) > 0:
+    print("Try again")
+else:
+    bisection_method(a,b, n)
