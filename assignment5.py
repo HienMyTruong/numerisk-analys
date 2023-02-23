2a529bce65107bb4f7e54891a694203a56cc6adb
Hien Truong <hien.truong2002.ht@gmail.com>
2023-02-23 20:26:19 +0100

    assignment 5 exercise 2

--- assignment5.py
+++ assignment5.py
@@ -23,31 +23,75 @@ def modifiedGramSchmidt(A):
 
 
 def gram_schmidt(X):
-    O = np.zeros(X.shape)
+    vec = np.zeros(X.shape)
     for i in range(X.shape[1]):
         # orthogonalization
         vector = X[:, i]
-        space = O[:, :i]
+        space = vec[:, :i]
         projection = vector @ space
         vector = vector - np.sum(projection * space, axis=1)  # normalization
         norm = np.sqrt(vector @ vector)
         vector /= abs(norm) < 1e-8 and 1 or norm
 
-        O[:, i] = vector
-    return O
-
-"""
-vectors = np.array([
-    [1, 1, 2, 0, 1, 1],
-    [0, 0, 0, 1, 2, 1],
-    [1, 2, 3, 1, 3, 2],
-    [1, 0, 1, 0, 1, 1]
-], dtype=float)
-"""
+        vec[:, i] = vector
+    return vec
 
 
-vectors = np.array([[4, 0], [3, 1]], dtype=float)
-orthonormal = gram_schmidt(vectors)
 
-print(orthonormal)
+def exercise1():
+    v1_ort = np.array([4 / 5, 3 / 5])
+    v1 = np.array([4, 3])
+    v2 = np.array([0, 1])
 
+    print(np.dot(v1_ort, v2))
+    v2_ort = (v2 - np.dot(v1_ort, v2) * v1_ort)
+    v2_ort = v2_ort/np.linalg.norm(v2_ort)
+    print("v2_ort = " + str(v2_ort) + "\n")
+
+    vectors = np.array([[4, 0], [3, 1]], dtype=float)
+    orthonormal = gram_schmidt(vectors)
+
+    q_actual = np.concatenate((v1_ort, v2_ort), axis=0)
+    print("expected Q = " + str(orthonormal))
+    print("actual Q: " + "q1= "+ str(v1_ort) + "   "+ "q2= "+ str(v2_ort) + "\n")
+
+
+    print("\n")
+    r_12 = np.dot(v1_ort, v2)
+
+    print("r_12 = " + str(r_12) + "\n")
+
+    r_matrix = np.array([[5, 3 / 5], [0, 4 / 5]], dtype=float)
+
+    print("R matrix = "+ str(np.matmul(orthonormal, r_matrix)) + "\n")
+
+def exercise2():
+
+
+    q_matrix = np.array([
+        [-0.4743, 0.2927, -0.5741, -0.2224, 0.5377, -0.1455],
+        [-0.3162, 0.0197, -0.4939, 0.0623, -0.8056, -0.0527],
+        [-0.7906, -0.2138, 0.5404, -0.0801, -0.0390, -0.0857],
+        [-0.1581, 0.1414, -0.0225, 0.9645, 0.1548, -0.0133],
+        [0, -0.9209, -0.3273, 0.0972, 0.1869, -0.0190],
+        [-0.1581, 0.0099, -0.0692, -0.0216, 0.0387, 0.9839]
+    ],dtype=float)
+
+    r_matrix = np.array([
+        [6.3246, -6.7989],
+        [0, 7.6010],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0]
+    ],dtype=float)
+
+    print(f"{q_matrix.shape=}, {r_matrix.shape=}")
+    a_matrix = np.matmul(q_matrix, r_matrix)
+
+    return a_matrix
+
+
+
+exercise1()
+print(exercise2())
